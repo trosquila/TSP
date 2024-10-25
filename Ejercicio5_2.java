df06bcc280996a23095272278592cf1954067f5f
Diego201120 <diego@clase.es>
2024-10-25 14:02:11 +0200

    j

--- Ejercicio5_2.java
+++ Ejercicio5_2.java
@@ -5,12 +5,14 @@ public class Ejercicio5_2 {
         Teoria teoria1 = new Teoria("Modelo de 3 Etapas de Lewin", "Lewin", "Dividido en 3 etapas que buscan ententer y getionar el cambio", 1940, "en uso","modelo","compuesto por 3 etapas","compuesto por tres fases descongelamiento, cambio, recongelamiento","realizar un cambio con tres pasos");
         Teoria teoria2 = new Teoria("Modelo de Cambio Planeado de Lippitt", "Lippitt", "Descompone el cambio en siete etapas, desde el diagnóstico del problema hasta la estabilización del cambio.", 1958, "en desuso","modelo","Se enfoca en el proceso de cambio planificado con un enfoque en la dinámica de las relaciones","Diagnóstico, Evaluación de Motivación, Definición de Progresos, Actuar como agente de cambio, Estabilización, Seguimiento, Finalización","Guiar a las organizaciones a través de un cambio estructurado y mantener la estabilidad post-cambio");
         Teoria teoria3 = new Teoria("Teoría del Cambio de Everett Rogers", "Rogers", "Explica cómo las innovaciones se difunden en una organización o sociedad, desde los innovadores hasta los rezagados.", 1962, "en uso","teoría","Se centra en cómo los individuos y grupos adoptan nuevas ideas o tecnologías.","Innovadores, Adoptadores tempranos, Mayoría temprana, Mayoría tardía, Rezagados.","Facilitar la adopción de innovaciones a través de la comprensión de los factores que afectan su difusión.");
-        
+        Teoria teoria4 = new Teoria("Modelo de Transición de Bridges", "Bridges", "Centrado en la transición en lugar del cambio", 1980, "en uso", "modelo", "compuesto por tres etapas", "Terminar, Zona Neutral, Nuevo Comienzo", "Ayudar a las personas a dejar atrás el pasado y asumir nuevos comportamientos o formas de pensar"
+
 
         // Imprimimos la información de las teorías
         System.out.println(teoria1);
         System.out.println(teoria2);  
-        System.out.println(teoria3);
+        System.out.println(teoria3);  
+        System.out.println(teoria4);
     }
 }
 
